2742c96e714443d62412c1f65906ffe7ed12896a
Fede-Coder <federiconieto04@gmail.com>
2023-02-08 23:47:49 -0300

    Added/Modified onSearch (Fetch)

--- src/App.js
+++ src/App.js
@@ -4,44 +4,25 @@ import Cards from './components/Cards'
 import React from 'react'
 import Nav from './components/Nav'
 
-const chars = [
-  {
-    id: 1,
-    name: "Rick Sanchez",
-    species: "Human",
-    gender: "Male",
-    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-  },
-  {
-    id: 2,
-    name: "Morty Smith",
-    species: "Human",
-    gender: "Male",
-    image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-  },
-  {
-    id: 3,
-    name: "Summer Smith",
-    species: "Human",
-    gender: "Female",
-    image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
-  },
-  {
-    id: 4,
-    name: "Beth Smith",
-    species: "Human",
-    gender: "Female",
-    image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
-  },
-];
-
 function App () {
 
   const [characters, setCharacters] = React.useState([]);
   const [inputChar, setInputChar] = React.useState('');
 
+  const onSearch = (id) => {
+    fetch(`https://rickandmortyapi.com/api/character/${id}`)
+      .then((res) => res.json())
+      .then((data) => 
+        {
+          data.name?setCharacters([...characters, data]):alert('No hay personajes con ese ID')
+        }
+      );
+  }
+
   const handleAddChar = () => {
-    setCharacters(characters.concat(chars.filter(char => char.id == inputChar)))
+    //setCharacters([...characters, ...chars.filter(char => char.id == inputChar)])
+    //setCharacters(characters.concat(chars.filter(char => char.id == inputChar)))
+    onSearch(inputChar)
   }
 
   const handleChange = (e) => {
