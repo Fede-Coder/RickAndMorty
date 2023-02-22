e6bc5760caa8be85249155f4523f9fef12bb1d5d
Fede-Coder <federiconieto04@gmail.com>
2023-02-21 23:36:50 -0300

    Added new bar for cards

--- src/components/Card/Cards.jsx
+++ src/components/Card/Cards.jsx
@@ -1,15 +1,40 @@
 import Card from './Card';
-import { motion } from 'framer-motion';
+import { CardsDivMain, CardsDivBar, CardsDivAddChar, CardsDivFilter, CardsDivSearch, CardsInput, CardsButton, CardsSelect } from './StyleCards';
 
 export default function Cards(props) {
    const { characters } = props;
    return (
-      <motion.div initial={{opacity: 0}} animate={{opacity:1}} exit={{opacity:0}}>
+      <CardsDivMain>
+         <CardsDivBar>
+            <CardsDivAddChar>
+               <CardsInput placeholder={'Id'} />
+               <CardsButton><i className="fa-solid fa-user-plus"></i></CardsButton>
+               <CardsButton><i className="fa-solid fa-shuffle"></i></CardsButton>
+            </CardsDivAddChar>
+            <CardsDivFilter>
+               <CardsSelect>
+                  <option value={''} selected disabled>Ordenar...</option>
+                  <option value={'1'}>Ascendente</option>
+                  <option value={'2'}>Descendente</option>
+               </CardsSelect>               
+               <CardsSelect>
+                  <option value={''} selected disabled>Genero</option>
+                  <option>Masculino</option>
+                  <option>Femenino</option>
+               </CardsSelect>
+               <CardsButton><i className="fa-solid fa-filter"></i></CardsButton>
+               <CardsButton><i className="fa-solid fa-rotate"></i></CardsButton>
+            </CardsDivFilter>
+            <CardsDivSearch>
+               <CardsInput placeholder={'Buscar...'}/>
+               <CardsButton><i className="fa-solid fa-magnifying-glass"></i></CardsButton>
+            </CardsDivSearch>
+         </CardsDivBar>
          {
             characters.map((character, index) => 
                <Card key={index} id={character.id} name={character.name} species={character.species} gender={character.gender} image={character.image} onClose={props.onClose} />
             )
          }
-      </motion.div>
+      </CardsDivMain>
    );
 }
---
+++ src/components/Card/StyleCards.jsx
@@ -0,0 +1,96 @@
+import styled from "styled-components";
+
+const CardsDivMain = styled.div`
+`
+
+const CardsDivBar = styled.div`
+    width: 85%;
+    margin: 0 auto;
+    background-color: white;
+    border-radius: 50px;
+    display: flex;
+    justify-content: space-evenly;
+    padding: 10px;
+    margin-bottom: 10px;
+    border-bottom: 5px solid #b3b3b3;
+`
+
+const CardsDivAddChar = styled.div`
+`
+
+const CardsDivFilter = styled.div`
+`
+
+const CardsDivSearch = styled.div`
+`
+
+const CardsInput = styled.input`
+    border-radius: 15px;
+    font-size: 20px;
+    border: none;
+    background-color: #e7e7e7;
+    outline: none;
+    text-align: center;
+    color: #707070;
+    padding: 10px 15px;
+    transition: 0.1s all linear;
+    
+    &:focus {
+        outline: 2px solid #379c35;
+        //box-shadow: 0 0 0px 2px #379c35;
+        //box-shadow: 0px 2px #379c35;
+    }
+`
+
+const CardsButton = styled.button`
+    font-size: 20px;
+    margin: 0 5px 0 5px;
+    background-color: #3cc03a;
+    padding: 10px 12px;
+    border: none;
+    border-radius: 15px;
+    cursor: pointer;
+    transition: 0.1s all linear;
+
+    i {
+        color: white;
+    }
+
+    &:hover {
+        background-color: #242424;
+    }
+`
+
+const CardsSelect = styled.select`
+    font-size: 20px;
+    border-radius: 15px;
+    border: none;
+    background-color: #e7e7e7;
+    text-align: center;
+    color: #707070;
+    padding: 10px 15px;
+    transition: 0.1s all linear;
+    margin-left: 5px;
+
+    option {
+        background-color: #e7e7e7;
+        &:first-child {
+            font-size: 16px;
+            font-weight: bold;
+            color: #999999
+        }
+    }
+`
+
+
+
+export {
+    CardsDivMain,
+    CardsDivBar,
+    CardsDivAddChar,
+    CardsDivFilter,
+    CardsDivSearch,
+    CardsInput,
+    CardsButton,
+    CardsSelect,
+}
